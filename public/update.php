4ddd5a5a0797852461cd242c97fb2a6797f1fbfb
Julien Vanier <jvanier@gmail.com>
2015-10-04 21:15:12 -0400

    Build and deploy

--- public/update.php
+++ public/update.php
@@ -7,10 +7,6 @@ chdir("..");
 echo "<h4>git pull</h4>\n"
 $output = shell_exec("git pull 2>&1");
 echo "<pre>$output</pre>\n";
-
-echo "<h4>npm install</h4>\n"
-$output = shell_exec("npm install 2>&1");
-echo "<pre>$output</pre>\n";
 ?>
 </body>
 </html>
